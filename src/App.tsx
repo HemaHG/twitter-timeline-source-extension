c642b422519d4ed23b02eac3f0d1e8f713820ac5
HemaHG <53568062+HemaHG@users.noreply.github.com>
2022-10-13 12:01:11 +0530

    Initialize add-license-1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          A very awesome Step Extension.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
